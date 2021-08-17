18668947b5a310059182858d10455f7328d52777
Jose Ribeiro <joseribeirocsr@gmail.com>
2021-08-17 19:24:40 -0300

    relacionando instructor com member

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -23,35 +23,7 @@ module.exports = {
                 return res.send("Please, fill all fields!")
             }
         }
-
-        // const query = `
-        //     INSERT INTO instructors (
-        //         name,
-        //         avatar_url,
-        //         gender,
-        //         services,
-        //         birth,
-        //         created_at
-        //     ) VALUES ($1, $2, $3, $4, $5, $6)
-        //     RETURNING id
-        // `
-
-        // const values = [
-        //     req.body.name,
-        //     req.body.avatar_url,
-        //     req.body.gender,
-        //     req.body.services,
-        //     date(req.body.birth).iso,
-        //     date(Date.now()).iso
-        // ]
-
-        // db.query(query, values, function(err, results) {
-        //     // console.log(err)
-        //     // console.log(results)
-        //     if(err) return res.send("Database Error")
-
-        //     return res.redirect(`/instructors/${results.rows[0].id}`)
-
+        
         Instructor.create(req.body, function(instructor) {
             return res.redirect(`/instructors/${instructor.id}`)
         })
